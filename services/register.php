23deae40a1a9bc0c21dd109999fc6b8640e052e4
Michael <gnimee@gmail.com>
2017-05-02 10:52:45 +0200

    Changes by milly

--- services/register.php
+++ services/register.php
@@ -1,7 +1,15 @@
 <?php
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
-   $dataBase = file_get_contents("services/users.txt")
+    $asUsers = file_get_contents("services/users.txt");
+    $ajUsers = json_decode($asUsers);
 
 
+    array_push($ajUsers , $jUser);
+
+
+    $asUsers = json_encode($ajUsers);
+    file_put_contents("../services/user.txt", $asUsers);
 
 ?>
